7bd7fd9d1215772339c928e8e965b66df8392bae
Henrik <henrik.lehtonen@gmail.com>
2017-09-28 13:58:25 +0200

    Initial server and test client

---
+++ example/client.js
@@ -0,0 +1,32 @@
+import jot from 'json-over-tcp';
+import { TEST_TYPE } from '../messageTypes';
+
+const PORT = 8099;
+
+const testMessage = {
+  messageType: TEST_TYPE,
+  name: 'test kikkula',
+};
+
+// Creates one connection to the server when the server starts listening
+function createConnection() {
+  // Start a connection to the server
+  var socket = jot.connect(PORT, function() {
+    // Send the initial message once connected
+    socket.write(testMessage);
+  });
+
+  // Whenever the server sends us an object...
+  socket.on('data', function(data) {
+    // Output the answer property of the server's message to the console
+    console.log("Server's answer: " + JSON.stringify(data));
+
+    // Wait one second, then write a question to the socket
+    setTimeout(function() {
+      // Notice that we "write" a JSON object to the socket
+      socket.write(testMessage);
+    }, 1000);
+  });
+}
+
+createConnection();
---
+++ server.js
@@ -0,0 +1,28 @@
+import jot from 'json-over-tcp';
+import testHandler from './handlers/test';
+import { TEST_TYPE } from './messageTypes';
+
+const PORT = 8099;
+
+var server = jot.createServer({}, 5000);
+server.on('connection', connectionHandler);
+
+// Triggered whenever something connects to the server
+function connectionHandler(socket) {
+  // Whenever a connection sends us an object...
+  socket.on('data', async data => {
+    if (!data.messageType) {
+      socket.write('Invalid or empty messageType');
+    }
+    console.log('data.messageType: ' + data.messageType);
+    switch (data.messageType) {
+      case TEST_TYPE:
+        const response = await testHandler(data);
+        socket.write(response);
+        return;
+    }
+  });
+}
+
+// Start listening
+server.listen(PORT);
