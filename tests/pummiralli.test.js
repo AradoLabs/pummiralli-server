0f6ec228bdb1502d279071921585a9a5d74b99cd
Mikko Junnila <mikko.junnila@gmail.com>
2017-10-09 01:09:35 +0300

    fix test

--- tests/pummiralli.test.js
+++ tests/pummiralli.test.js
@@ -1,12 +1,13 @@
+import net from "net";
 import Pummiralli from "../domain/pummiralli";
 
 test("collects message correctly", () => {
   const ralli = new Pummiralli();
-  ralli.collectMessage({
+  ralli.collectMessage(new net.Socket(), {
     messageType: "join",
     data: {
       name: "test",
     },
   });
-  expect(ralli.messages.length).toBe(1);
+  expect(ralli.eventsReceived.length).toBe(1);
 });
